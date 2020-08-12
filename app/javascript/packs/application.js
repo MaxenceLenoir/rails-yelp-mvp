cc5079f49890f6399cfd39916397ea1a9c51f931
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-08-12 17:44:52 +0200

    Exercise 2 Stylé

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 import 'bootstrap';
+import { dynamicRating } from "../plugins/starsInReviewForm";
 
+dynamicRating();
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
